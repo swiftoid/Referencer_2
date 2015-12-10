58a14e1bae7e0c07ccbf47a071c30f4db539ec01
Swiftoid <keepitcash@hotmail.com>
2015-12-10 21:57:35 +0000

    Added new some color blocks

--- src/com/center_pannel.java
+++ src/com/center_pannel.java
@@ -4,16 +4,16 @@ import com.bookingSnake.referencer.main;
 
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.Color;
 /**
  * Created by Andy on 10/12/2015.
  */
 public class center_pannel extends JFrame {
 
 
-    private JLabel label;
+    private JLabel label_1;
     private JButton button;
-    private TextField textfield;
+    private TextField textfield_1;
     private JLabel label_2;
     private TextField  textfield_2;
     private JLabel label_3;
@@ -22,33 +22,43 @@ public class center_pannel extends JFrame {
     private TextField textfield_4;
     private JLabel label_5;
     private TextField textfield_5;
-
-
+    private JPanel color_1 = new JPanel();
+    private JPanel color_2 =new JPanel ();
 
 
     public center_pannel () {
 
+        color_1.setBackground(Color.BLUE);
+        add(color_1);
         setLayout(new FlowLayout());
-        label = new JLabel("Enter Author Details");
-        add(label);
-        textfield = new TextField(20);
-        add(textfield);
+
+        color_2.setBackground(Color.CYAN);
+        add(color_2);
+        label_1 = new JLabel("Enter Author Details");
+        add(label_1);
+        textfield_1 = new TextField(20);
+        add(textfield_1);
+
         label_2 = new JLabel("Enter Title of Publiction");
         add(label_2);
         textfield_2 = new TextField(20);
         add(textfield_2);
+
         label_3 = new JLabel("Enter Date of Publication");
         add(label_3);
         textfield_3 = new TextField (20);
         add(textfield_3);
+
         label_4 = new JLabel("Enter Publisher");
         add(label_4);
         textfield_4 = new TextField(20);
         add(textfield_4);
+
         label_5 = new JLabel("Enter Place of Publication");
         add(label_5);
         textfield_5 = new TextField (20);
         add(textfield_5);
+
         button = new JButton("Create reference");
         add(button);
     }
@@ -58,9 +68,8 @@ public class center_pannel extends JFrame {
 
 
         center_pannel pa = new center_pannel();
-
         pa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        pa.setBackground(Color.BLUE);
         pa.setVisible(true);
         pa.setSize(250, 360);
 
